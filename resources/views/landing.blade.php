866c93388381ac273a3419a63ae1f59d9e27768b
ryankananyola <mariadestan2027@gmail.com>
2025-10-11 10:15:34 +0700

    update landing

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -7,17 +7,14 @@
 
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;500;700&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Twinkle+Star&display=swap" rel="stylesheet">
-
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css" rel="stylesheet">
-
     <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
 
     <link rel="stylesheet" href="{{ asset('css/landing.css') }}">
     <link rel="stylesheet" href="{{ asset('css/footer.css') }}">
-
 </head>
-<body>
 
+<body>
     <header class="navbar">
         <div class="logo">Bloomify</div>
         <nav>
@@ -71,6 +68,10 @@
         AOS.init({
             duration: 1000,
             once: true
+        });
+
+        window.addEventListener('load', () => {
+            AOS.refresh();
         });
     </script>
 </body>
